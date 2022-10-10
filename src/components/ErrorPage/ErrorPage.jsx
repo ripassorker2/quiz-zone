041cc6e2c2b19cf0ed697b133328b6998ac0d3ac
Ripassorker <ripassorker02@glail.com>
2022-10-11 00:02:00 +0600

    added error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const ErrorPage = () => {
   return (
     <div>
-      {/* <section className="flex items-center bg-gray-200  h-screen p-16 dark:bg-gray-900 dark:text-gray-100">
+      <section className="flex items-center bg-gray-200  h-screen p-16 dark:bg-gray-900 dark:text-gray-100">
         <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
           <div className="max-w-md text-center">
             <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
@@ -26,7 +26,7 @@ const ErrorPage = () => {
             </Link>
           </div>
         </div>
-      </section> */}
+      </section>
     </div>
   );
 };
