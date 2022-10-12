2f997d629d131cb009accce18e2173ea74725ce9
Ripassorker <ripassorker02@glail.com>
2022-10-12 15:04:13 +0600

    change rechart

--- README.md
+++ README.md
@@ -6,10 +6,10 @@ This is quiz application. You can paractice here. Here are javascript, css , git
 
 ## Steps :
 
-1. Frist create a react app using `npx create-react-app ` this command.
-   2.I am add recct react router in this app. Using this command `npm install react-router-dom`.
-   3.I am addded some react pacakge like react icons, react toastify, react rechatrs, etc.
-   4.After that i create all components step by steps.
+1.Frist create a react app using `npx create-react-app ` this command.
+2.I am add recct react router in this app. Using this command `npm install react-router-dom`.
+3.I am addded some react pacakge like react icons, react toastify, react rechatrs, etc.
+4.After that i create all components step by steps.
 
 ## This projects is very interesting for me.
 
--- src/components/Recharts/Rechart.jsx
+++ src/components/Recharts/Rechart.jsx
@@ -1,13 +1,6 @@
 import React, { useContext } from "react";
 // import { QuizContext } from "../Root/Root";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend } from "recharts";
 import { CoursesContext } from "../../Main/Main";
 
 const Rechart = () => {
@@ -16,19 +9,15 @@ const Rechart = () => {
   return (
     <div>
       <div className="flex text-center m-auto justify-center md:w-3/4 my-7 text-white">
-        <LineChart width={400} height={400} data={quizData}>
-          <Line
-            type="monotone"
-            dataKey="total"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-
-          <CartesianGrid strokeDasharray="1 1" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-        </LineChart>
+        <div>
+          <BarChart width={450} height={440} data={quizData}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Legend />
+            <Tooltip />
+            <Bar dataKey="total" fill="gray" />
+          </BarChart>
+        </div>
       </div>
     </div>
   );
